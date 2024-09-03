3ed6efa500af5ab2be7c409c109df69e1ac315ba
vadimalygin <vadim.malygin.2001@mail.ru>
2024-09-03 14:41:40 +0700

    Добавленны условный оператор и операторы цикла

--- pr001.java
+++ pr001.java
@@ -9,5 +9,31 @@ class pr001 {
 		System.out.println("Результат целочисленного деления: " + var2/2);
 		double var3;
 		System.out.println("Результат дробного деления: " + (double)var2/2);
+		System.out.println();
+
+		//Пример использования оператора if
+		/*if(var1 = 0){
+			System.out.println("Условие вар1=0 выполнено");
+		} небулевый результат присвоения*/
+		if(var1/2 == 512){
+                        System.out.println("Условие \"var1/2=512\" выполнено");
+                }
+		if(true){
+                        System.out.println("\tУсловие true \n\tвыполнено");
+                }
+		System.out.println("Строка проверки");
+		System.out.println();
+		
+		//Циклы
+		for (int i = 0; i<10; i++){
+			System.out.println("Строка № " + i);
+		}
+		//i = 10 Переменная i локальная и существует только во время выполнения цикла
+		System.out.println();
+		int j;
+		for (j = 0; j<10; j++){
+                        System.out.print(j + " ");
+                }
+		System.out.println();
 	}
 }
