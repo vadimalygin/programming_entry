13e5d6330a8ffb6f11a0aa452ab0d14a9d0f9b2f
vadimalygin <vadim.malygin.2001@mail.ru>
2024-09-03 14:11:34 +0700

    Добавленны примеры деления

--- pr001.java
+++ pr001.java
@@ -1,5 +1,13 @@
 class pr001 {
 	public static void main(String args[]){
-		System.out.println("Первая программа на Java");
+		System.out.println("Первая программа на Java"); //Выводим строку на консоль
+		int var1 = 1024;
+		int var2;
+		//System.out.println(var2); ошибка: переменной не присвоенно значение
+		var2 = 411;
+		System.out.println(var1/2);
+		System.out.println("Результат целочисленного деления: " + var2/2);
+		double var3;
+		System.out.println("Результат дробного деления: " + (double)var2/2);
 	}
 }
