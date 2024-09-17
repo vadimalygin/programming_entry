88f9f53957070073c233a29b28a91275db54b2d0
vadimalygin <vadim.malygin.2001@mail.ru>
2024-09-17 13:58:57 +0700

    Добавлена программа чтения с клавиатуры

---
+++ pr003.java
@@ -0,0 +1,8 @@
+class  pr003 {
+	public static void main(String args[]) throws java.io.IOException {
+		char ch;
+		System.out.print("нажмите любую клавишу на клавиатуре и нажмите ENTER ");
+		ch = (char) System.in.read();
+		System.out.println("Вы нажали: " + ch);
+	}
+}
