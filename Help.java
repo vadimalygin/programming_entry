02cf9047af0cbe62485a5862e012baaa108508bd
vadimalygin <vadim.malygin.2001@mail.ru>
2024-09-24 15:08:31 +0700

    Добавленно описание циклов, изменено поведение программы Добавили обработку выбора пользователя в случае ошибочного выбора содержимое меню показывается занового

--- Help.java
+++ Help.java
@@ -1,31 +1,60 @@
 class Help {
         public static void main(String args[]) throws java.io.IOException {
-                char choice;
-                System.out.println("Справка по операторам");
-                System.out.println("1. if");
-                System.out.println("2. switch");
-                System.out.println("Выберите номер раздела: ");
+                char choice, ignore;
+		do{
+                	System.out.println("Справка по операторам");
+                	System.out.println("1. if");
+                	System.out.println("2. switch");
+			System.out.println("3. for");
+			System.out.println("4. while");
+			System.out.println("5. do-while");
+                	System.out.println("Выберите номер раздела: ");
 
-                choice = (char) System.in.read();
+                	choice = (char) System.in.read();
+			do{
+				ignore = (char)System.in.read();
+			}while(ignore!='\n');
+		
 
                 System.out.println("\n");
                 switch(choice) {
                         case '1' :
                                 System.out.println("опертатор if\n");
-                                System.out.println("if (условие) оператор");
-                                System.out.println("else оператор");
+                                System.out.println("if (условие) {\n\tоператор\n}");
+                                System.out.println("[else {\n\tоператор\n}]");
                                 break;
                         case '2':
                                 System.out.println("Оператор switch\n");
-                                System.out.println("Switch (выражение) {");
-                                System.out.println("case константа: ");
-                                System.out.println(" последовательность операторов");
-                                System.out.println(" break;");
-                                System.out.println(" . . . ");
+                                System.out.println("switch (выражение) {");
+                                System.out.println("\tcase константа: ");
+                                System.out.println("\tпоследовательность операторов");
+                                System.out.println("\tbreak;");
+                                System.out.println("\t. . . ");
                                 System.out.println("} ");
                                 break;
+			case '3':
+                                System.out.println("Оператор цикла for\n");
+                                System.out.println("for ([инициализация];[выражение];[конец иттерации]) {");
+                                System.out.println("\t[тело цикла]");
+                                System.out.println("} ");
+                                break;
+			case '4':
+                                System.out.println("Оператор цикла while\n");
+                                System.out.println("while ([выражение]) {");
+                                System.out.println("\t[тело цикла]");
+                                System.out.println("} ");
+                                break;
+			case '5':
+                                System.out.println("Оператор цикла do-while\n");
+                                System.out.println("do {");
+                                System.out.println("\t[тело цикла]");
+                                System.out.println("}while([выражение]);");
+                                break;
+				
                         default:
                                 System.out.println("Неверный выбор раздела");
 		}
+		}while(choice<'1'|choice>'5');
+
 	}
 }
