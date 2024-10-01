a8c126693b675ea5eddfd177b05848ec5592bf57
vadimalygin <vadim.malygin.2001@mail.ru>
2024-10-01 15:04:24 +0700

    Добавили новый экземпляр класса

--- pr004.java
+++ pr004.java
@@ -11,17 +11,23 @@ class pr004 {
 	public static void main(String args[]){
 		//Creating an object
 		//
-		auto lada = new auto();
+		auto lada = new auto() , porsche = new auto();
 		double milage;
 
 		//setting values for objects variables
 		//
 		lada.pass_num = 4;
 		lada.tank = 40;
-		lada.fuel_usage = 9.5;
+		lada.fuel_usage = 5.5;
+
+		porsche.pass_num = 1;
+		porsche.tank = 100;
+		porsche.fuel_usage = 14.0;
 
 		//calculation of milage for lada then fuel tank is full
 		milage = lada.tank / lada.fuel_usage *100;
-		System.out.println("Lada will pass out after " + milage + " kilometers");
+		System.out.println("Lada will pass out after " + (int)(milage) + " kilometers");
+		milage = porsche.tank / porsche.fuel_usage *100;
+		System.out.println("Porsche will pass out after " + (int)(milage) + " kilometers");
 	}
 }
