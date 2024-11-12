282c80519914640e330ba65b3d78f7df8ae90208
vadimalygin <vadim.malygin.2001@mail.ru>
2024-11-12 13:07:56 +0700

    Added methods that use various number of argument and their overload

--- pr008.java
+++ pr008.java
@@ -40,6 +40,97 @@ class StaticBlock{
 	}
 }
 
+class Outer {
+	int[] nums;
+	Outer(int[] n){
+		nums = n;
+	}
+
+	void analyze() {
+		Inner inOb = new Inner();
+		System.out.println("Minimum: " + inOb.min());
+		System.out.println("Maximum: " + inOb.max());
+		System.out.println("Mean:    " + inOb.mean());
+	}
+
+	class Inner {
+		int min() {
+			int m = nums[0];
+			for (int i = 1; i< nums.length; i++){
+				if(nums[i] < m) m = nums[i];
+			}
+			return m;
+		}
+
+		int max() {
+                        int m = nums[0];
+                        for (int i = 1; i< nums.length; i++){
+                                if(nums[i] > m) m = nums[i];
+                        }
+                        return m;
+                }
+
+		int mean() {
+			int summ = 0;
+			for (int i = 0; i< nums.length; i++){
+				summ += nums[i]; 
+			}
+			return summ / nums.length;
+		}
+	}
+}
+
+class VarArgs{
+	static void vaTest (int ... v) {
+		System.out.println("Number of arguments: " + v.length);
+		System.out.println("Containtments of an array: ");
+
+		for (int i = 0; i < v.length; i++){
+			System.out.println("Argument #"+ i + ": "+ v[i]);
+		}
+	}
+
+	static void vaTest2 (String msg, int ... v) {
+		System.out.println("Received message: " + msg);
+                System.out.println("Number of arguments: " + v.length);
+                System.out.println("Containtments of an array: ");
+
+                for (int i = 0; i < v.length; i++){
+                        System.out.println("Argument #"+ i + ": "+ v[i]);
+                }
+        }
+
+	static void vaTest3 (int ... v) {
+                System.out.println("\nvaTest3 method type: int");
+		System.out.println("Number of arguments: " + v.length);
+                System.out.println("Containtments of an array: ");
+
+                for (int i = 0; i < v.length; i++){
+                        System.out.println("Argument #"+ i + ": "+ v[i]);
+                }
+        }
+
+	static void vaTest3 (boolean ... v) {
+		System.out.println("\nvaTest3 method type: boolean");
+                System.out.println("Number of arguments: " + v.length);
+                System.out.println("Containtments of an array: ");
+
+                for (int i = 0; i < v.length; i++){
+                        System.out.println("Argument #"+ i + ": "+ v[i]);
+                }
+        }
+
+	static void vaTest3 (String ... v) {
+                System.out.println("\nvaTest3 method type: String");
+                System.out.println("Number of arguments: " + v.length);
+                System.out.println("Containtments of an array: ");
+
+                for (int i = 0; i < v.length; i++){
+                        System.out.println("Argument #"+ i + ": "+ v[i]);
+                }
+        }
+}
+
 class pr008{
 	public static void main(String args[]){
 		Factorial f = new Factorial();
@@ -78,6 +169,61 @@ class pr008{
 
 		StaticBlock ob3 = new StaticBlock("New instance of 'StaticBlock'");
 		System.out.println("Square root of 2: " + StaticBlock.root2 + "\nSquare root of 3: " + StaticBlock.root3);
+		
+		System.out.println("\nExample of class nesting");
+		int[] arr = {5, 6, -9, 10, 50, -67};
+		Outer ob4 = new Outer(arr);
+		System.out.println("Outer ob4.analyze:"); 
+		ob4.analyze();
+		System.out.println("\nExample of class creation inside main program");
+		////////////////////////
+		class ShowBt {
+        		private int numbits;
+
+        		ShowBt (int n){numbits = n;}
+
+        		void show(long val){
+                		long mask = 1;
+                		//Leftshift value 1
+                		mask <<=numbits-1;
+
+                		int spacer = 0;
+                		for(;mask != 0; mask >>>= 1){
+                        		if ((val & mask) != 0)  {System.out.print("1");}
+                        		else                    {System.out.print("0");}
+                        		spacer++;
+                        		if ((spacer%8) == 0)    {System.out.print(" "); spacer = 0;}
+                		}
+        		}
+		}
+		///////////////////
+	
+		for (byte b = 0; b < 10; b++){
+			ShowBt byteval = new ShowBt(8);
+			System.out.print("\n" + b + " in binary equals: ");
+			byteval.show(b);
+
+		}
+
+		System.out.println("\nExample of class method using various argument nuber:");
+
+		VarArgs.vaTest();
+		VarArgs.vaTest(10);
+		VarArgs.vaTest(11, 12, 13, 14);
+
+		System.out.println("\nExample of class method using various argument nuber along with regular arguments:");
+
+                VarArgs.vaTest2("test");
+                VarArgs.vaTest2("test", 10);
+                VarArgs.vaTest2("test", 11, 12, 13, 14);
+
+		System.out.println("\nExample of using overload on class method using various argument nuber:");
+
+                //VarArgs.vaTest3(); //does not compile due to ambiguos expression
+                VarArgs.vaTest3(11, 12, 13);
+                VarArgs.vaTest3(true, false, false);
+		VarArgs.vaTest3("msg1","msg2","msg3");
+
 	}
 	
 }
