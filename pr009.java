ca80e4971e70485eb5f6981c337e2ca944ecbd33
vadimalygin <167408022+vadimalygin@users.noreply.github.com>
2024-12-02 19:46:28 +0700

    mistakes were made...

--- pr009.java
+++ pr009.java
@@ -1,7 +1,41 @@
 // class relationships
 
 class _2dShape {
-	double width, height;
+	private double width, height;
+	int common;
+	
+	_2dShape(){
+		width = height = 0;
+	}
+
+	_2dShape(double p){
+                width = height = p;
+        }
+
+
+	_2dShape (double w, double h){
+		width = w;
+		height = h;
+	}
+
+	double getWidth(){
+		return width;
+	}
+
+	double getHeight(){
+                return height;
+        }
+
+	void setWidth(double w){
+                if (w > 200.0) width = 200.0;
+		else width = w;
+	}
+
+	void setHeight(double h){
+                if (h > 290.0) height = 290.0;
+                else height = h;
+        }
+
 	void showDim() {
 		System.out.println("Width & height = " + width + " & " + height);
 	}
@@ -9,9 +43,31 @@ class _2dShape {
 
 class Triangle extends _2dShape {
 	String style;
+	int common;
+
+	Triangle() {
+                super();
+                style = "none";
+        }
+
+	Triangle(int a, int b){
+		super.common = a;
+		common = b;
+		System.out.println("super.common = " + super.common + "\ncommon inside class = " + common);
+	}
+
+	Triangle(double p) {
+                super(p);
+                style = "filled";
+        }
+	
+	Triangle(double w, double h, String s) {
+		super(w, h);
+		style = s;
+	}
 
 	double area(){
-		return width * height / 2;
+		return getWidth() * getHeight() / 2;
 	}
 
 	void showStyle() {
@@ -19,17 +75,53 @@ class Triangle extends _2dShape {
 	}
 }
 
-class pr009 {
+class Rectangle extends _2dShape {
+	String outline;
+	int common;
+
+	Rectangle () {
+                super();
+                outline = "none";
+        }
+
+	Rectangle(int a, int b){
+                super.common = a;
+                common = b;
+		System.out.println("super.common = " + super.common + "\ncommon inside class = " + common);
+        }
+
+	Rectangle (double p) {
+                super(p);
+                outline = "solid";
+        }
+
+	Rectangle (double w, double h, String o) {
+		super(w, h);
+                outline = o;
+        }
+
+	double area() {
+		return getWidth() * getHeight();
+	}
+
+	void showOutline() {
+                System.out.println("Outline: " + outline);
+        }
+
+	boolean isSquare() {
+		if(getWidth() == getHeight()) return true;
+		return false;
+	}
+}
+
+public class pr009 {
 	public static void main(String args[]){
-		Triangle t1 = new Triangle();
-		Triangle t2 = new Triangle();
-
-		t1.height = t1.width = 4.0;
-		t1.style = "filled";
-
-		t2.width = 8.0;
-		t2.height = 12.0;
-		t2.style = "bordered";
+		Triangle t1 = new Triangle(4.0);
+		Triangle t2 = new Triangle(8.0, 12.0, "bordered");
+		Triangle t3 = new Triangle();
+		Triangle t4 = new Triangle(5, 10);
+		
+		t3 = t2; //pseudoname for t2 is t3
 
 		System.out.println("\nt1 info:\nArea: " + t1.area());
 		t1.showDim();
@@ -38,5 +130,26 @@ class pr009 {
 		System.out.println("\nt2 info:\nArea: " + t2.area());
                 t2.showDim();
                 t2.showStyle();
+
+		System.out.println("\nt3 info:\nArea: " + t3.area());
+                t3.showDim();
+                t3.showStyle();
+
+		Rectangle r1 = new Rectangle(4.0);
+		Rectangle r2 = new Rectangle(8.0, 12.0, "dash");
+		Rectangle r3 = new Rectangle();
+
+		System.out.println("\nr1 info:\nArea: " + r1.area() + "\nIs a square: " + r1.isSquare());
+                r1.showDim();
+                r1.showOutline();
+
+
+                System.out.println("\nr2 info:\nArea: " + r2.area() + "\nIs a square: " + r2.isSquare());
+                r2.showDim();
+                r2.showOutline();
+
+		System.out.println("\nr3 info:\nArea: " + r3.area() + "\nIs a square: " + r3.isSquare());
+                r3.showDim();
+                r3.showOutline();
 	}
 }
